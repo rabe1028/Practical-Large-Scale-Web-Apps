a05fefe8a9b2461841384aa061dce9fbb654bb09
Ryo Abe <30817722+rabe1028@users.noreply.github.com>
2021-05-14 14:40:20 +0900

    add PicturePropertyRepository

--- mojipic/app/controllers/PicturesController.scala
+++ mojipic/app/controllers/PicturesController.scala
@@ -3,9 +3,11 @@ package controllers
 import java.nio.file.{FileSystems, Files, Path, StandardCopyOption}
 import java.time.{Clock, LocalDateTime}
 import javax.inject.{Inject, Singleton}
-
 import com.google.common.net.MediaType
+import com.redis.RedisClient
 import domain.entity.{PictureProperty, TwitterId}
+import domain.repository.PicturePropertyRepository
+import infrastructure.redis.RedisKeys
 import play.api.cache.SyncCacheApi
 import play.api.libs.Files.TemporaryFile
 import play.api.mvc._
@@ -19,6 +21,8 @@ class PicturesController @Inject()(
                                     clock: Clock,
                                     executionContext: ExecutionContext,
                                     val cache: SyncCacheApi,
+                                    picturePropertyRepository: PicturePropertyRepository,
+                                    redisClient: RedisClient,
                                   ) extends TwitterLoginController(cc) {
 
   implicit val ec = executionContext
@@ -36,8 +40,11 @@ class PicturesController @Inject()(
             Files.copy(file.ref.path, originalFilepath, StandardCopyOption.COPY_ATTRIBUTES)
             val propertyValue = createPicturePropertyValue(TwitterId(accessToken.getUserId), file, form, originalFilepath)
 
-            println(propertyValue)
-            // TODO MySQLへのプロパティ保存とRedisへのタスクの保存
+            val pictureId = picturePropertyRepository.create(propertyValue)
+            pictureId.map { (id) =>
+              redisClient.rpush(RedisKeys.Tasks, id.value)
+              Ok("Picture uploaded.")
+            }
 
             Future.successful(Ok("Picture uploaded."))
           case _ => Future.successful(Unauthorized("Need picture data."))
